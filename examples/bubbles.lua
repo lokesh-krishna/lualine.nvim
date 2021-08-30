d2357ee2b182b7c97f4d3f93d207f453829ea480
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-08-30 21:07:24 +0600

    fix lint issues and remove default option values

--- examples/bubbles.lua
+++ examples/bubbles.lua
@@ -1,69 +1,45 @@
 -- Bubbles config for lualine
 -- Author: lokesh-krishna
-local lualine = require 'lualine'
+-- MIT license, see LICENSE for more details.
 
-
--- Config
-local config = {
-	options = {
-        icons_enabled = true,
-		disabled_filetypes = {'NvimTree'},
-		theme = 'moonfly',
-		component_separators = {'|', '|'},
-		section_separators = {'', ''},
+require'lualine'.setup {
+  options = {
+    theme = 'moonfly',
+    component_separators = '|',
+    section_separators = {'', ''},
   },
-	sections = {
-		lualine_a = {
-			{
-			'mode',
-			separator = {'', ''},
-			right_padding = 2,
-			left_padding = 1,
-			}
-		},
-            lualine_b = {
-			{
-			'filename',
-			right_padding = 2,
-			left_padding = 2,
-		},
-			{ 
-			'branch',
-			right_padding = 2,
-			left_padding = 2,
-			}
-		},
-		lualine_c = {'fileformat'},
-		lualine_x = {},
-		lualine_y = {
-			{
-			'filetype',
-			right_padding = 2,
-			left_padding = 2,
-			},
-			{
-			'progress',
-			right_padding = 2,
-			left_padding = 2,
-			} 
-		},
-    		lualine_z = {
-	    		{
-	    		'location',
-	    		separator = {'', ''},
-            		left_padding = 2,
-            		right_padding = 1,
-	    		}
-	    	},
-  	},
-  	inactive_sections = {
-    		lualine_a = {'filename'},
-    		lualine_b = {},
-    		lualine_c = {},
-    		lualine_x = {},
-    		lualine_y = {},
-    		lualine_z = {'location'}
-  	},
-  	tabline = {},
-  	extensions = {}
+  sections = {
+    lualine_a = {
+      {'mode',
+       separator = {'', ''},
+       right_padding = 2,
+      }
+    },
+    lualine_b = {
+      {'filename', padding = 2},
+      {'branch', padding = 2,}
+    },
+    lualine_c = {'fileformat'},
+    lualine_x = {},
+    lualine_y = {
+      {'filetype', padding = 2},
+      {'progress', padding = 2}
+    },
+    lualine_z = {
+      {'location',
+       separator = {'', ''},
+       left_padding = 2,
+      }
+    },
+  },
+  inactive_sections = {
+    lualine_a = {'filename'},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'location'}
+  },
+  tabline = {},
+  extensions = {}
 }
